f583fdba933bb6c2fb2a76c843d41aa3d3b27646
BenCloughessy <110562848+BenCloughessy@users.noreply.github.com>
2023-01-10 08:19:29 -0500

    add button spacing, cleaner equation display, auto reset

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
 import './App.css';
-import { Card, CardTitle, Button, CardHeader, Row, Col, ButtonGroup } from 'reactstrap';
+import { Card, Button, CardHeader, ButtonGroup } from 'reactstrap';
 import { useState } from 'react';
 
 function App() {
-  const [leftOperand, setLeftOperand] = useState(null)
-  const [rightOperand, setRightOperand] = useState(null)
-  const [operator, setOperator] = useState(null)
+  const [leftOperand, setLeftOperand] = useState('')
+  const [rightOperand, setRightOperand] = useState('')
+  const [operator, setOperator] = useState('')
   const [result, setResult] = useState(null)
 
   const equation = `${leftOperand}${operator}${rightOperand}`
+  
+  
 
   const setOperand = (operand) => {
-    if(leftOperand === null) {
+    if(leftOperand === '') {
+      setResult(null)
       setLeftOperand(operand)
     } else {
       setRightOperand(operand)
     }
   }
 
+  const resetEquation = () => {
+    setLeftOperand('')
+    setRightOperand('')
+    setOperator('')
+  }
+
   const calculate = () => {
     switch(operator) {
       case '+':
         setResult(leftOperand + rightOperand)
+        resetEquation()
         break;
       case '-':
         setResult(leftOperand - rightOperand)
+        resetEquation()
         break;
       case '*':
         setResult(leftOperand * rightOperand)
+        resetEquation()
         break;
       case '/':
         setResult(leftOperand / rightOperand)
+        resetEquation()
         break;
       default:
         // code block
@@ -43,28 +56,28 @@ function App() {
       <Card className='calculator mx-auto'>
         <CardHeader className='displayScreen'>{result === null ? equation : result}</CardHeader>
 
-        <ButtonGroup>
-          <Button onClick={() => setOperand(0)}>0</Button>
+        <ButtonGroup className='mb-1'>
+          <Button className='mx-1' onClick={() => setOperand(0)}>0</Button>
           <Button onClick={() => setOperand(1)}>1</Button>
-          <Button onClick={() => setOperand(2)}>2</Button>
+          <Button className='mx-1' onClick={() => setOperand(2)}>2</Button>
           <Button onClick={() => setOperand(3)}>3</Button>
-          <Button onClick={() => setOperand(4)}>4</Button>
+          <Button className='mx-1'onClick={() => setOperand(4)}>4</Button>
         </ButtonGroup>
 
         <ButtonGroup>
-          <Button onClick={() => setOperand(5)}>5</Button>
+          <Button className='mx-1' onClick={() => setOperand(5)}>5</Button>
           <Button onClick={() => setOperand(6)}>6</Button>
-          <Button onClick={() => setOperand(7)}>7</Button>
+          <Button className='mx-1' onClick={() => setOperand(7)}>7</Button>
           <Button onClick={() => setOperand(8)}>8</Button>
-          <Button onClick={() => setOperand(9)}>9</Button>
+          <Button className='mx-1' onClick={() => setOperand(9)}>9</Button>
         </ButtonGroup>
         
         <Card className='operators mx-auto'>
-          <ButtonGroup>
+          <ButtonGroup className='mb-1 mx-1'>
             <Button onClick={() => setOperator('+')}>+</Button>
-            <Button onClick={() => setOperator('-')}>-</Button>
+            <Button className='mx-1' onClick={() => setOperator('-')}>-</Button>
             <Button onClick={() => setOperator('*')}>*</Button>
-            <Button onClick={() => setOperator('/')}>/</Button>
+            <Button className='ms-1'onClick={() => setOperator('/')}>/</Button>
           </ButtonGroup>
           <ButtonGroup>
             <Button onClick={() => calculate()}>=</Button>
@@ -79,3 +92,42 @@ function App() {
 }
 
 export default App;
+
+// /**
+//  * @param {character[][]} board
+//  * @return {boolean}
+//  */
+// var isValidSudoku = function(board) {
+//   let isValid = true
+
+//   // Check Row
+//   for (let i = 0; i < board.length; i++) {    // Loop thru big array
+      
+//       for (let t = 0; t < board[i].length; t++) {     // loop thru each row
+//           let rowArr = []     // Create row array
+
+//           // Populate rowArr with row values if it is a number, and check if already included
+//           if ((rowArr.includes(board[i][t])) && (!board[i][t]) === ".") {
+//               return isValid = false
+//           } else if((!board[i][t]) === ".") {
+//               rowArr.push(board[i][t])
+//           }
+//       }
+//   }
+
+//   // Check Col
+//   for (let i = 0; i < board.length; i++) {    // Loop thru big array
+//       let col = [[]]
+
+//       for (let t = 0; t < board[i].length; t++) {     // loop thru each row
+
+//           // Save each value to respective col array if its a number
+//           if (!board[i][t] === ".") {
+//               col[t].push(board[i][t])
+//           }
+
+//       }
+//   }
+
+//   // Check Sub-Boxes
+// };
